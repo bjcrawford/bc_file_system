f8d1aa0044cc0ff11a584c0d3a4b9630241afc58
Brett Crawford <brettjamescrawford@gmail.com>
2014-11-21 18:18:25 -0500

    Added files to hold the file operations

---
+++ src/bc_file_operations.c
@@ -0,0 +1,15 @@
+/**
+ * @file bc_file_operations.c
+ * @author Brett Crawford
+ * @brief Constructing a File System and the Related IOCS Functions
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-12-05
+ *  Description: 
+ *     This file holds all of the operations that can be performed 
+ *     on the file sectors of the virtual drive.
+*/
+
+#include "bc_file_operations.h"
---
+++ src/bc_file_operations.h
@@ -0,0 +1,18 @@
+/**
+ * @file bc_file_operations.h
+ * @author Brett Crawford
+ * @brief Constructing a File System and the Related IOCS Functions
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-12-05
+ *  Description: 
+*/
+
+#ifndef BC_FILE_OPERATIONS
+#define BC_FILE_OPERATIONS
+
+
+
+#endif
